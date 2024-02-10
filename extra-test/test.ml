6203499a7431ffbd980aee9bd695a44bc5b374c7
Shayne Fletcher <shayne@shaynefletcher.org>
2024-02-10 11:26:04 -0500

    add first extra test

--- extra-test/test.ml
+++ extra-test/test.ml
@@ -1,13 +1,12 @@
 open OUnit2
 
-let rec sum = function
-  | [] -> 0
-  | x :: xs -> x + sum xs
+let test_strip_prefix_0 _ =
+  match String.strip_prefix_s "foo" "foobar" with
+  | Some "bar" -> assert true
+  | _ -> assert false
 
-let tests = "test suite for sum" >::: [
-  "empty" >:: (fun _ -> assert_equal 0 (sum []));
-  "singleton" >:: (fun _ -> assert_equal 1 (sum [1]));
-  "two_elements" >:: (fun _ -> assert_equal 3 (sum [1; 2]));
+let tests = "test suite for extra" >::: [
+      "strip_prefix0" >:: test_strip_prefix_0;
 ]
 
 let _ = run_test_tt_main tests
