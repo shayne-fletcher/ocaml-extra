b20de99075d15403f9d6b5f20665f427fb47f341
Shayne Fletcher <shayne@shaynefletcher.org>
2024-02-10 11:28:19 -0500

    run tests in workflow

--- extra-test/test.ml
+++ extra-test/test.ml
@@ -1,12 +1,12 @@
 open OUnit2
 
-let test_strip_prefix_0 _ =
+let test_strip_prefix_s_0 _ =
   match String.strip_prefix_s "foo" "foobar" with
   | Some "bar" -> assert true
   | _ -> assert false
 
 let tests = "test suite for extra" >::: [
-      "strip_prefix0" >:: test_strip_prefix_0;
+      "strip_prefix_s_0" >:: test_strip_prefix_s_0;
 ]
 
 let _ = run_test_tt_main tests
