3d5d1ee107206ce40f9b6985eea63c64a8ab87ca
Shayne Fletcher <shayne@shaynefletcher.org>
2024-02-18 18:14:57 -0500

    more testse

--- extra-test/test.ml
+++ extra-test/test.ml
@@ -17,11 +17,19 @@ let test_replace_s_3 _ =
 let test_isspace_0 _ =
   assert(String.isspace ' ')
 
-let test_drop_while_0 _ =
-  assert_equal [3; 4] (List.drop_while (fun i -> i < 3) [1; 2; 3; 4])
-
 let test_take_while_0 _ =
   assert_equal [1; 2] (List.take_while (fun i -> i < 3) [1; 2; 3; 4])
+let test_take_while_1 _ =
+  assert_equal [] (List.take_while (fun i -> i < 3) [])
+let test_take_while_2 _ =
+  assert_equal [2; 4; 6] (List.take_while (fun i -> i mod 2  == 0) [2; 4; 6; 7; 8])
+
+let test_drop_while_0 _ =
+  assert_equal [3; 4] (List.drop_while (fun i -> i < 3) [1; 2; 3; 4])
+let test_drop_while_1 _ =
+  assert_equal [] (List.drop_while (fun i -> i < 3) [])
+let test_drop_while_2 _ =
+  assert_equal [7; 8] (List.drop_while (fun i -> i mod 2  == 0) [2; 4; 6; 7; 8])
 
 let test_break_0 _ =
   assert_equal ([1; 2], [3; 4]) (List.break (fun i -> i >= 3) [1; 2; 3; 4])
@@ -33,15 +41,26 @@ let test_drop_while_end_0 _ =
 
 let tests = "test suite for extra" >::: [
       "strip_prefix_s_0" >:: test_strip_prefix_s_0;
+
       "replace_s_0" >:: test_replace_s_0;
       "replace_s_1" >:: test_replace_s_1;
       "replace_s_2" >:: test_replace_s_2;
       "replace_s_3" >:: test_replace_s_3;
+
       "is_space_0" >:: test_isspace_0;
-      "drop_while_0" >:: test_drop_while_0;
+
       "take_while_0" >:: test_take_while_0;
+      "take_while_1" >:: test_take_while_1;
+      "take_while_2" >:: test_take_while_2;
+
+      "drop_while_0" >:: test_drop_while_0;
+      "drop_while_1" >:: test_drop_while_1;
+      "drop_while_2" >:: test_drop_while_2;
+
       "break_0" >:: test_break_0;
+
       "take_while_end_0" >:: test_take_while_end_0;
+
       "drop_while_end_0" >:: test_drop_while_end_0;
 ]
 
