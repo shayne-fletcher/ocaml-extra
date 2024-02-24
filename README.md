e452165496f4e8b636e3de51e49d31a535cfe18a
Shayne Fletcher <shayne@shaynefletcher.org>
2024-02-24 17:04:39 -0500

    rule for ocamldoc

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # extra [![Buck build and test](https://github.com/shayne-fletcher/ocaml-extra/actions/workflows/buck-build-and-test.yml/badge.svg)](https://github.com/shayne-fletcher/ocaml-extra/actions/workflows/buck-build-and-test.yml)
 
 Programming with [dotslash](https://github.com/facebook/dotslash) + [buck2](https://buck2.build/) + [opam](https://opam.ocaml.org/) + [Core](https://opensource.janestreet.com/core/). OCaml ports
-of Neil Mitchell's Haskell package
-[extra](https://hackage.haskell.org/package/extra).
+of [Neil Mitchell's extra](https://hackage.haskell.org/package/extra).
--- extra-lib/string.mli
+++ extra-lib/string.mli
@@ -1,18 +1,17 @@
 val strip_infix_s : string -> string -> (string * string) option
 val strip_suffix_s : string -> string -> string option
 val strip_prefix_s : string -> string -> string option
-
 val replace_s : string -> string -> string -> string
 
-(* [repeatedly_s f str] applies [f] as many times as necessary to
+val repeatedly_s : (string -> 'b * string) -> string -> 'b list
+(** [repeatedly_s f str] applies [f] as many times as necessary to
    reduce [str] to a list of things *)
-val repeatedly_s : (string -> ('b * string)) -> string -> 'b list
 
-(* [isspace c] tests if [c] is a space char. *)
 val isspace : char -> bool
+(** [isspace c] tests if [c] is a space char. *)
 
-(* [trim_start s] strip leading space from s. *)
-val trim_start: string -> string
+val trim_start : string -> string
+(** [trim_start s] strip leading space from s. *)
 
-(* [word1 s] to split the first word of a string *)
-val word1: string -> (string * string)
+val word1 : string -> string * string
+(** [word1 s] to split the first word of a string *)
